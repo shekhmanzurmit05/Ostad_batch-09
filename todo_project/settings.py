4083f5de4af4ed0d50ada529aa2ecafe1c44b82f
shekhmanzurmit05 <shekhmanzurmit05@gmail.com>
2026-02-20 17:31:07 +0600

    Add STATIC_ROOT setting for Render deployment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -139,3 +139,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
